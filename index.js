38f4eb3a442818641412fe83b99b4cbffba8eafd
michael <4apaev@gmail.com>
2017-03-11 19:37:08 +0200

    declared deps

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 global.log = console.log.bind(console)
 
-const app = require('../srv')()
+const app = require('miniserver')()
 const Borks = require('./routes/borks')
 const GeoDB = require('./db/geo')
 const Conf = app.Conf = require('./config.json')
@@ -19,9 +19,9 @@ DB.connect(Conf.HOST)
 
   .then(() =>
     app
-      .use(require('../srv/middleware/logger')('statusCode', 'method', 'url'))
-      .use(require('../srv/middleware/cors'))
-      .post(require('../srv/middleware/body'))
+      .use(require('miniserver/middleware/logger')('statusCode', 'method', 'url'))
+      .use(require('miniserver/middleware/cors'))
+      .post(require('miniserver/middleware/body'))
       .get(Conf.ROUTE, Borks.list)
       .post(Conf.ROUTE, Borks.add)
       .listen(Conf.PORT, () => log(`running on localhost:`, Conf.PORT)))
