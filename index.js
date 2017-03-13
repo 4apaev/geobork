538ab66fcafb2c6eee6940948de8578c4857dd8f
michael <4apaev@gmail.com>
2017-03-14 01:57:25 +0200

    use ENV vars instead config file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 global.log = console.log.bind(console)
 
+const { PORT, MONGO_HOST } = process.env;
 const app = require('miniserver')()
 const Borks = require('./routes/borks')
 const GeoDB = require('./db/geo')
@@ -13,7 +14,7 @@ const DB = app.DB = new GeoDB({
   field: Conf.FIELD
 });
 
-DB.connect(Conf.HOST)
+DB.connect(MONGO_HOST)
   .then(() =>
         DB.index2d(Conf.NAME))
 
@@ -22,12 +23,21 @@ DB.connect(Conf.HOST)
       .use(require('miniserver/middleware/logger')('statusCode', 'method', 'url'))
       .use(require('miniserver/middleware/cors'))
       .post(require('miniserver/middleware/body'))
+
+      .get('/', (req, res) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end('ok');
+      })
+
       .get(Conf.ROUTE, Borks.list)
       .post(Conf.ROUTE, Borks.add)
-      .listen(Conf.PORT, () => log(`running on localhost:`, Conf.PORT)))
+      .listen(PORT, () => log(`
+==========================
+  connected to ${ MONGO_HOST }
+  running on localhost:${ PORT }
+==========================
+    `)))
 
   .catch(err => {
     log(err)
-    process.exit(1)
   });
-
